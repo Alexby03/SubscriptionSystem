558d95d5851e4e41b8d615e3551e275479d259c1
Alexby03 <alexanderby03@gmail.com>
2025-09-10 23:27:40 +0200

    Fixed Subscription -> Invoice -> Payment relationship.

--- SubscriptionSystem.Api/obj/Debug/net9.0/SubscriptionSystem.Api.AssemblyInfo.cs
+++ SubscriptionSystem.Api/obj/Debug/net9.0/SubscriptionSystem.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubscriptionSystem.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2042f848b75567e125f2f67fb9df768fc53a58a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08deb77f99a77340490db4e89c6de87084a1e9a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubscriptionSystem.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubscriptionSystem.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SubscriptionSystem.Models/Models/Invoice.cs
+++ SubscriptionSystem.Models/Models/Invoice.cs
@@ -7,29 +7,27 @@ public class Invoice
 {
     public Guid InvoiceId { get; private set; } = Guid.NewGuid();
     public Guid CustomerId { get; private set; }
+    public Guid SubscriptionId { get; private set; }
     public DateTime IssueDate { get; private set; } = DateTime.UtcNow;
     public DateTime DueDate { get; private set; }
     public DateTime PaidDate { get; private set; }
     public decimal Amount { get; private set; }
     public InvoiceStatus Status { get; private set; }
-    public Guid? PaymentId { get; private set; }
 
-    public Invoice(DateTime dueDate, decimal amount, Guid customerId, Guid? payment = null)
+    public Invoice(DateTime dueDate, decimal amount, Guid customerId, Guid subscriptionId)
     {
         DueDate = dueDate;
         Amount = amount;
         Status = InvoiceStatus.Pending;
         CustomerId = customerId;
-        PaymentId = payment;
+        SubscriptionId = subscriptionId;
     }
 
-    public PayInvoiceResult MarkPaid(Guid paymentId) //TODO
+    public PayInvoiceResult MarkPaid()
     {
-        if (PaymentId != null) return PayInvoiceResult.AlreadyPaid;
-        PaymentId = paymentId;
+        if (Status == InvoiceStatus.Paid) return PayInvoiceResult.AlreadyPaid;
         PaidDate = DateTime.UtcNow;
         Status = InvoiceStatus.Paid;
         return PayInvoiceResult.Success;
     }
-
 }
